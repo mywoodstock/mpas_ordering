f0c42cbe2acd0c171e766fc51941e26b35e2091c
Abhinav Sarje <abhinav.sarje@gmail.com>
2013-12-13 10:55:50 -0800

    option to give file name prefix

--- mpas_order.cpp
+++ mpas_order.cpp
@@ -3,7 +3,7 @@
  *
  *  File: mpas_order.cpp
  *  Created: Nov 12, 2013
- *  Modified: Wed 11 Dec 2013 11:46:26 AM PST
+ *  Modified: Fri 13 Dec 2013 10:23:10 AM PST
  *
  *  Author: Abhinav Sarje <asarje@lbl.gov>
  */
@@ -15,19 +15,19 @@
 int main(int narg, char** args) {
 	char* part_filename;
 
-	if(narg == 4) {
-		part_filename = args[3];
-	} else if(narg == 3) {
+	if(narg == 5) {
+		part_filename = args[4];
+	} else if(narg == 4) {
 		part_filename = "";
 	} else {
-		std::cout << "usage: mpas_order <grid_file> <graph_file> [<partition_file>]" << std::endl;
+		std::cout << "usage: mpas_order <grid_file> <graph_file> <output_prefix> [<partition_file>]" << std::endl;
 		return 1;
 	} // if-else
 
 	MPASElementOrder my_ordering(args[1], args[2], part_filename);
 	my_ordering.reorder_elements(XYZ_SORT);
 	//my_ordering.print_all();
-	my_ordering.save_elements_order("neworder");
+	my_ordering.save_elements_order(args[3]);
 
 	return 0;
 } // main()
